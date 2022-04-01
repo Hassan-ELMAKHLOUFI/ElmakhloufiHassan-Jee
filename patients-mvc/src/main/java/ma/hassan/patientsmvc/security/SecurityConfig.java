42451c27da43b9fc41307d30f42e85b3f0a38f55
Hassan-ELMAKHLOUFI <hassanelmakhloufi2000@gmail.com>
2022-04-01 18:06:00 +0000

    Update SecurityConfig.java

--- patients-mvc/src/main/java/ma/hassan/patientsmvc/security/SecurityConfig.java
+++ patients-mvc/src/main/java/ma/hassan/patientsmvc/security/SecurityConfig.java
@@ -1,10 +1,14 @@
 package ma.hassan.patientsmvc.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import sun.security.util.Password;
 
 @Configuration
 @EnableWebSecurity
@@ -13,13 +17,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("{noop}user1").password("1234").roles("user").and()
-                .withUser("admin1").password("1234").roles("admin","user");
+        auth.inMemoryAuthentication().withUser("user1").password(passwordEncoder().encode("12345")).roles("user").and()
+                .withUser("admin1").password(passwordEncoder().encode("12345")).roles("admin","user");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
        http.formLogin();
        http.authorizeHttpRequests().anyRequest().authenticated();
+    }
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
