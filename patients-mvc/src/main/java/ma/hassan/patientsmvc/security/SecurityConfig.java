c1cfa7cf8f9cd82e4aba0908ac8c54f0d02abc3f
Hassan-ELMAKHLOUFI <hassanelmakhloufi2000@gmail.com>
2022-04-01 19:41:36 +0000

    exception handling

--- patients-mvc/src/main/java/ma/hassan/patientsmvc/security/SecurityConfig.java
+++ patients-mvc/src/main/java/ma/hassan/patientsmvc/security/SecurityConfig.java
@@ -25,6 +25,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
        http.formLogin();
        http.authorizeHttpRequests().anyRequest().authenticated();
+       http.exceptionHandling().accessDeniedPage("/403");
     }
     @Bean
     PasswordEncoder passwordEncoder(){
