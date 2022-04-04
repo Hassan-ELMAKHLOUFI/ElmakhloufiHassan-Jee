28d642bda1646bf07667c4eea39ea99c795bd7d0
Hassan-ELMAKHLOUFI <hassanelmakhloufi2000@gmail.com>
2022-04-04 11:14:24 +0000

    Update PatientsMvcApplication.java

--- patients-mvc/src/main/java/ma/hassan/patientsmvc/PatientsMvcApplication.java
+++ patients-mvc/src/main/java/ma/hassan/patientsmvc/PatientsMvcApplication.java
@@ -2,6 +2,7 @@ package ma.hassan.patientsmvc;
 
 import ma.hassan.patientsmvc.entities.Patient;
 import ma.hassan.patientsmvc.repository.PatientRepository;
+import ma.hassan.patientsmvc.security.service.SecurityService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,14 +17,29 @@ public class PatientsMvcApplication {
         SpringApplication.run(PatientsMvcApplication.class, args);
     }
 
-    @Bean
-    CommandLineRunner commandLineRunner(PatientRepository patientRepository){
+   // @Bean
+//    CommandLineRunner commandLineRunner(PatientRepository patientRepository){
+//        return args -> {
+//            patientRepository.save(new Patient(null,"hassan",new Date(),false,12));
+//            patientRepository.save(new Patient(null,"Mohammed",new Date(),false,12));
+//            patientRepository.save(new Patient(null,"hassan",new Date(),true,12));
+//            patientRepository.save(new Patient(null,"ilyass",new Date(),false,12));
+//            patientRepository.save(new Patient(null,"yassine",new Date(),false,12));
+//
+//        };
+//    }
+    //@Bean
+    CommandLineRunner saveUsers(SecurityService securityService){
         return args -> {
-            patientRepository.save(new Patient(null,"hassan",new Date(),false,12));
-            patientRepository.save(new Patient(null,"Mohammed",new Date(),false,12));
-            patientRepository.save(new Patient(null,"hassan",new Date(),true,12));
-            patientRepository.save(new Patient(null,"ilyass",new Date(),false,12));
-            patientRepository.save(new Patient(null,"yassine",new Date(),false,12));
+            securityService.saveNewUser("mohamed","1234","1234");
+            securityService.saveNewUser("yassmine","1234","1234");
+            securityService.saveNewUser("hassan","1234","1234");
+            securityService.saveNewRole("user","");
+            securityService.saveNewRole("admin","");
+            securityService.addRoleToUser("mohamed","user");
+            securityService.addRoleToUser("mohamed","admin");
+            securityService.addRoleToUser("yassmine","user");
+            securityService.addRoleToUser("hassan","user");
 
         };
     }
