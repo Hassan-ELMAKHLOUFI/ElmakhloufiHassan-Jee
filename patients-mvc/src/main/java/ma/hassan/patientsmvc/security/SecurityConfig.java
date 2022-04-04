55bc8cab7a5c91cfd10c4f5f80522fdfb3c2c735
Hassan-ELMAKHLOUFI <hassanelmakhloufi2000@gmail.com>
2022-04-04 01:47:41 +0000

    Update SecurityConfig.java

--- patients-mvc/src/main/java/ma/hassan/patientsmvc/security/SecurityConfig.java
+++ patients-mvc/src/main/java/ma/hassan/patientsmvc/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package ma.hassan.patientsmvc.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -10,15 +11,27 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import sun.security.util.Password;
 
+import javax.sql.DataSource;
+
 @Configuration
 @EnableWebSecurity
 
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    DataSource dataSource;
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("user1").password(passwordEncoder().encode("12345")).roles("user").and()
-                .withUser("admin1").password(passwordEncoder().encode("12345")).roles("admin","user");
+        System.out.println(passwordEncoder().encode("12345"));
+
+        //auth.inMemoryAuthentication().withUser("user1").password(passwordEncoder().encode("12345")).roles("user").and()
+        //        .withUser("admin1").password(passwordEncoder().encode("12345")).roles("admin","user");
+        auth.jdbcAuthentication().dataSource(dataSource)
+                .usersByUsernameQuery("select username as principal ,password as credentials,active from users where username=? ")
+                .authoritiesByUsernameQuery("select username principal ,role as user_roles from users_roles where username= ?")
+                .rolePrefix("ROLE_")
+                .passwordEncoder(passwordEncoder());
+
     }
 
     @Override
